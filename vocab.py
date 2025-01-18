0a3691e6c3b54199e788fad5dca8cfc70ea84781
johncle <90406181+johncle@users.noreply.github.com>
2025-01-18 02:45:53 -0500

    begin refactoring of vocab data structure

--- vocab.py
+++ vocab.py
@@ -43,10 +43,184 @@ The Kotoba CSV file is sorted by ascending lesson number
 """
 import csv
 import re
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
+import bisect
 import argparse
 from openpyxl import load_workbook
 from utils import get_ranges
+
+
+class VocabEntries:
+    """Abstraction to make modifying the entries dict easier with less code duplication, but
+    is probably slower
+
+    Main reason is that lessons are automatically sorted upon insertion
+    """
+
+    def __init__(self):
+        """
+        dict structure:
+        { kanji: ( [<kana>], [<part of speech>], [<meaning>], [<lesson #>] ) }
+          str      list[str] list[str]           list[str]    list[str]
+        - If no kanji exists, use kana for key instead, and kana in value[0] stays the same
+        """
+        self._dict = defaultdict(lambda: ([], [], [], self.LessonList()))
+
+    def get_entries(self) -> dict[str, (list[str], list[str], list[str], list[str])]:
+        return self._dict
+
+    def add_entry(
+        self,
+        kanji: str,
+        kanas: list[str] | str,
+        parts: list[str] | str,
+        meanings: list[str] | str,
+        lessons: list[str] | str,
+    ) -> None:
+        assert kanas  # should never be None or empty
+        # allow inputting single str for ease of use, and convert to list
+        if isinstance(kanas, str):
+            kanas = [kanas]
+        if isinstance(parts, str):
+            parts = [parts]
+        if isinstance(meanings, str):
+            meanings = [meanings]
+        if isinstance(lessons, str):
+            lessons = [lessons]
+
+        # if kanji is empty, use the kana as the dict key
+        # don't use sanitized version since original might have additional info to display
+        if not kanji:
+            kanji = kanas[0]
+
+        self.__add_kanas(kanji, kanas)
+        self.__add_parts(kanji, parts)
+        self.__add_meanings(kanji, meanings)
+        self.__add_lessons(kanji, lessons)
+
+        # print(kanji, self._dict[kanji])
+
+    def modify_entry(
+        self,
+        kanji: str,
+        kanas: list[str] | str | None,
+        parts: list[str] | str | None,
+        meanings: list[str] | str | None,
+        lessons: list[str] | str | None,
+    ) -> None:
+        pass
+
+    def sort(self):
+        """Return sorted representation of dict by lesson sort order"""
+        ordered = dict(
+            sorted(
+                self._dict.items(), key=lambda entry: _lesson_sort_key(entry[1][3][0])
+            )
+        )
+        return ordered
+
+    def __add_kanas(self, kanji: str, kanas: list[str]) -> None:
+        kana_list = self._dict[kanji][0]
+        sanitized_kanas = self.__sanitize_kanas(kanas)
+        for kana in sanitized_kanas:
+            if kana not in kana_list:
+                kana_list.append(kana)
+
+    def __add_parts(self, kanji, parts: list[str]) -> None:
+        part_list = self._dict[kanji][1]
+        for part in parts:
+            if part not in part_list:
+                part_list.append(part.strip())
+
+    def __add_meanings(self, kanji, meanings: list[str]) -> None:
+        meaning_list = self._dict[kanji][2]
+        for meaning in meanings:
+            if meaning not in meaning_list:
+                meaning_list.append(meaning.strip())
+
+    def __add_lessons(self, kanji, lessons: list[str]) -> None:
+        lesson_list = self._dict[kanji][3]
+        for num in lessons:
+            if num not in lesson_list:
+                lesson_list.insert(num.strip())
+
+    def __sanitize_kanas(self, kanas: list[str]) -> list[str]:
+        """Some words have extra non-kana/non-katakana symbols which, at best, make it annoying to
+        answer. Other times, words inside of parentheses are used for clarification which means they
+        aren't even part of the reading. We remove all non-essential symbols from the answer for
+        better user experience
+
+        Note: '、' is interpreted by kotoba as a normal comma which separates an answer into multiple
+
+        Examples:
+            - Optional: 'おかえり（なさい）', 'ほ（う）っておく'
+            - Clarification: 'いう（もんくを）', 'あんぜん（な）'
+            - Tilde: 'ただの～', '～かな（あ）', 'もう～ない', '～か～'
+            - Ellipse: '（～は）…といういみだ'
+            - Alternatives: 'なん/なに'
+            - Negative: 'あまり ＋ negative'
+            - ???: '～（ん）だろう'
+        """
+        sanitized_kanas = []
+        for kana in kanas:
+            sanitized = kana
+            # remove anything within parentheses, misc characters, and ' ＋ negative'
+            sanitized = re.sub(r"（.*）|[～〜…！、]| ＋ negative", "", sanitized)
+            # split words separated by "/" (alternative readings)
+            sanitized = [skana.strip() for skana in sanitized.split("/")]
+            sanitized_kanas.extend(sanitized)
+        return sanitized_kanas
+
+    def __getitem__(self, index: int | slice):
+        # support indexing
+        return self._dict[index]
+
+    class LessonList:
+        """Provides simple interface for inserting into lesson list while maintaining order
+        from _lesson_sort_order():
+
+        Lesson number can have the following forms:
+        - 会L## (1-2 digits)
+        - 会L##(e)
+        - 読L##-I, 読L##-II, 読L##-III
+        - 会G
+            - Greetings lesson, we interpret this as lesson 0
+
+        We want to sort by ascending lesson number, but also need to account for 会L##(e) and 読L##-I.
+        For any number x, the sort order should look as below:
+            1. 会Lx
+            2. 会Lx(e)
+            3. 読Lx-I
+            4. 読Lx-II
+            5. 読Lx-III
+        """
+
+        def __init__(self):
+            self.key = _lesson_sort_key  # returns (int, int)
+            self._lessons: list[tuple[tuple[int, int], str]] = []  # (rank, lesson)
+
+        def insert(self, lesson: str):
+            """Insert lesson while maintaining lesson order"""
+            key_value = self.key(lesson)
+            bisect.insort(self._lessons, (key_value, lesson))
+
+        def __getitem__(self, index: int | slice):
+            # support indexing and slicing
+            if isinstance(index, slice):
+                return [item[1] for item in self._lessons[index]]
+            return self._lessons[index][1]
+
+        def __len__(self) -> int:
+            # len(length.size())
+            return len(self._lessons)
+
+        def __iter__(self):
+            # iterate over original values
+            return (item[1] for item in self._lessons)
+
+        def __repr__(self):
+            # represent as list of values
+            return repr([item[1] for item in self._lessons])
 
 
 def excel_to_dict(
@@ -69,7 +243,8 @@ def excel_to_dict(
     workbook = load_workbook(filename, read_only=True)
     sheet = workbook.active
 
-    vocab_dict = defaultdict(lambda: ([], [], [], []))
+    # vocab_dict = defaultdict(lambda: ([], [], [], []))
+    vocab_dict = VocabEntries()
     for row in sheet.iter_rows(min_row=11, values_only=True):
         # stop when reached end of sheet (for some reason doesn't stop automatically)
         if all(cell is None for cell in row):
@@ -77,32 +252,19 @@ def excel_to_dict(
 
         # currently extracting data specific to the sheet I'm using
         kana, kanji, part, meaning, lesson = row[1:6]
-        sanitized_kana = _sanitize_kana(kana)  # list
-        # if word is purely kana (no kanji), use it for both key and value[0]
-        if not kanji:
-            kanji = kana
-
-        # if kanji already seen, append to existing arrays
-        kana_list, part_list, meaning_list, lesson_list = vocab_dict[kanji]
-        # ignore duplicates
-        for sanitized in sanitized_kana:
-            if sanitized not in kana_list:
-                kana_list.append(sanitized.strip())
-        if part not in part_list:
-            part_list.append(part.strip())
-        if meaning not in meaning_list:
-            meaning_list.append(meaning.strip())
         # entry may be in multiple lessons that might not have been split yet
-        for num in lesson.split(","):
-            if num not in lesson_list:
-                lesson_list.append(num.strip())
+        vocab_dict.add_entry(kanji, kana, part, meaning, lesson.split(","))
 
     # make manual adjustments in place
     if adjustments_file:
-        _make_adjustments(vocab_dict, adjustments_file)
+        _make_adjustments(vocab_dict.get_entries(), adjustments_file)
     # sort by ascending lesson number (first in lesson list)
     ordered = dict(
-        sorted(vocab_dict.items(), key=lambda lesson: _lesson_sort_key(lesson[1][3][0]))
+        # sorted(vocab_dict.items(), key=lambda entry: _lesson_sort_key(entry[1][3][0]))
+        sorted(
+            vocab_dict.get_entries().items(),
+            key=lambda entry: _lesson_sort_key(entry[1][3][0]),
+        )
     )
     return ordered
 
@@ -110,7 +272,8 @@ def excel_to_dict(
 def _sanitize_kana(kana: str) -> list[str]:
     """Some words have extra non-kana/non-katakana symbols which, at best, make it annoying to
     answer. Other times, words inside of parentheses are used for clarification which means they
-    aren't even part of the reading. We remove all non-essential symbols for better user experience
+    aren't even part of the reading. We remove all non-essential symbols from the answer for better
+    user experience
 
     Note: '、' is interpreted by kotoba as a normal comma which separates an answer into multiple
 
@@ -127,7 +290,7 @@ def _sanitize_kana(kana: str) -> list[str]:
     # remove anything within parentheses, misc characters, and ' ＋ negative'
     sanitized_kana = re.sub(r"（.*）|[～〜…！、]| ＋ negative", "", sanitized_kana)
     # split words separated by "/" (alternative readings)
-    sanitized_kana = sanitized_kana.split("/")
+    sanitized_kana = [skana.strip() for skana in sanitized_kana.split("/")]
     return sanitized_kana
 
 
